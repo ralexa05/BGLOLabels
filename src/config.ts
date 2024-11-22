213b7e756bc412fe87a5688b55b8f38e5cd7a19e
ralexa05 <34200967+ralexa05@users.noreply.github.com>
2024-11-22 16:51:28 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config';
 
+function getNumberEnv(envVariable: string | undefined, defaultValue: number): number {
+  const value = Number(envVariable);
+  return isNaN(value) ? defaultValue : value;
+}
+
 export const DID = process.env.DID ?? '';
 export const SIGNING_KEY = process.env.SIGNING_KEY ?? '';
 export const HOST = process.env.HOST ?? '127.0.0.1';
-export const PORT = process.env.PORT ? Number(process.env.PORT) : 4100;
-export const METRICS_PORT = process.env.METRICS_PORT ? Number(process.env.METRICS_PORT) : 4101;
+export const PORT = getNumberEnv(process.env.PORT, 4100);
+export const METRICS_PORT = getNumberEnv(process.env.METRICS_PORT, 4101);
 export const FIREHOSE_URL = process.env.FIREHOSE_URL ?? 'wss://jetstream.atproto.tools/subscribe';
 export const WANTED_COLLECTION = 'app.bsky.feed.like';
 export const BSKY_IDENTIFIER = process.env.BSKY_IDENTIFIER ?? '';
 export const BSKY_PASSWORD = process.env.BSKY_PASSWORD ?? '';
-export const CURSOR_UPDATE_INTERVAL =
-  process.env.CURSOR_UPDATE_INTERVAL ? Number(process.env.CURSOR_UPDATE_INTERVAL) : 60000;
+export const CURSOR_UPDATE_INTERVAL = getNumberEnv(process.env.CURSOR_UPDATE_INTERVAL, 60000);
+
