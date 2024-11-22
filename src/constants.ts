6673281db24b8db371f392542882094ab15c5bfa
ralexa05 <34200967+ralexa05@users.noreply.github.com>
2024-11-21 21:56:38 -0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,42 +5,65 @@ export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
     rkey: 'insert-rkey-here',
-    identifier: 'earth',
+    identifier: 'alpha_phi_alpha',
     locales: [
-      { lang: 'en', name: 'Earth 🌎', description: 'Earth'},
-      { lang: 'pt-BR', name: 'Terra 🌎', description: 'Terra'},
+      { lang: 'en', name: 'ΑΦΑ', description: 'Historically Black Greek Letter organization founded on December 4, 1906, on the campus of Cornell University.'}
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'fire',
+    identifier: 'alpha_kappa_alpha',
     locales: [
-      { lang: 'en', name: 'Fire 🔥', description: 'Fire'},
-      { lang: 'pt-BR', name: 'Fogo 🔥', description: 'Fogo'},
+      { lang: 'en', name: 'ΑΚΑ', description: 'Historically Black Greek Letter organization founded on January 15, 1908, on the campus of Howard University.'}
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'air',
+    identifier: 'kappa_alpha_psi',
     locales: [
-      { lang: 'en', name: 'Air 💨', description: 'Air'},
-      { lang: 'pt-BR', name: 'Ar 💨', description: 'Ar'},
+      { lang: 'en', name: 'ΚΑΨ', description: 'Historically Black Greek Letter organization founded on January 5, 1911, on the campus of Indiana University Bloomington.'}
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'water',
+    identifier: 'omega_psi_phi',
     locales: [
-      { lang: 'en', name: 'Water 💧', description: 'Water'},
-      { lang: 'pt-BR', name: 'Água 💧', description: 'Água'},
+      { lang: 'en', name: 'ΩΨΦ', description: 'Historically Black Greek Letter organization founded on November 17, 1911, on the campus of Howard University.'}
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'love',
+    identifier: 'delta_sigma_theta',
     locales: [
-      { lang: 'en', name: 'Love 💞', description: 'Love'},
-      { lang: 'pt-BR', name: 'Amor 💞', description: 'Amor'},
+      { lang: 'en', name: 'ΔΣΘ', description: 'Historically Black Greek Letter organization founded on January 13, 1913, on the campus of Howard University.'}
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'phi_beta_sigma',
+    locales: [
+      { lang: 'en', name: 'ΦΒΣ', description: 'Historically Black Greek Letter organization founded on January 9, 1914, on the campus of Howard University.'}
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'zeta_phi_beta',
+    locales: [
+      { lang: 'en', name: 'ΖΦΒ', description: 'Historically Black Greek Letter organization founded on January 16, 1920, on the campus of Howard University.'}
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'sigma_gamma_rho',
+    locales: [
+      { lang: 'en', name: 'ΣΓΡ', description: 'Historically Black Greek Letter organization founded on November 12, 1922, on the campus of Butler University.'}
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'iota_phi_theta',
+    locales: [
+      { lang: 'en', name: 'ΙΦΘ', description: 'Historically Black Greek Letter organization founded on September 19, 1963, on the campus of Morgan State University.'}
     ]
   },
 ];
