8f0b30089f48cf3d2dd5bb1b9f7b07f076eaaf74
ralexa05 <34200967+ralexa05@users.noreply.github.com>
2024-11-22 01:17:42 -0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,69 +1,72 @@
 import { Label } from './types.js';
 
 export const DELETE = 'insert-rkey-of-delete-post-here';
-export const LABEL_LIMIT = 1;
+export const LABEL_LIMIT = ;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'alpha_phi_alpha',
-    locales: [
-      { lang: 'en', name: 'ΑΦΑ', description: 'Historically Black Greek Letter organization founded on December 4, 1906, on the campus of Cornell University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'alpha_kappa_alpha',
-    locales: [
-      { lang: 'en', name: 'ΑΚΑ', description: 'Historically Black Greek Letter organization founded on January 15, 1908, on the campus of Howard University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'kappa_alpha_psi',
-    locales: [
-      { lang: 'en', name: 'ΚΑΨ', description: 'Historically Black Greek Letter organization founded on January 5, 1911, on the campus of Indiana University Bloomington.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'omega_psi_phi',
-    locales: [
-      { lang: 'en', name: 'ΩΨΦ', description: 'Historically Black Greek Letter organization founded on November 17, 1911, on the campus of Howard University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'delta_sigma_theta',
-    locales: [
-      { lang: 'en', name: 'ΔΣΘ', description: 'Historically Black Greek Letter organization founded on January 13, 1913, on the campus of Howard University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'phi_beta_sigma',
-    locales: [
-      { lang: 'en', name: 'ΦΒΣ', description: 'Historically Black Greek Letter organization founded on January 9, 1914, on the campus of Howard University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'zeta_phi_beta',
-    locales: [
-      { lang: 'en', name: 'ΖΦΒ', description: 'Historically Black Greek Letter organization founded on January 16, 1920, on the campus of Howard University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'sigma_gamma_rho',
-    locales: [
-      { lang: 'en', name: 'ΣΓΡ', description: 'Historically Black Greek Letter organization founded on November 12, 1922, on the campus of Butler University.'}
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'iota_phi_theta',
-    locales: [
-      { lang: 'en', name: 'ΙΦΘ', description: 'Historically Black Greek Letter organization founded on September 19, 1963, on the campus of Morgan State University.'}
-    ]
-  },
-];
+  rkey: 'insert-rkey-here',
+  identifier: 'blackownedbusiness',
+  locales: [
+    { lang: 'en', name: 'Black Owned Business', description: 'User owns or promotes Black-owned businesses.' }
+  ]
+}
+
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackcontentcreators',
+  locales: [
+    { lang: 'en', name: 'Black Content Creator', description: 'User is a Black content creator.' }
+  ]
+}
+
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blerds',
+  locales: [
+    { lang: 'en', name: 'Blerd', description: 'User identifies as a Blerd (Black nerd).' }
+  ]
+}
+
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackauthors',
+  locales: [
+    { lang: 'en', name: 'Black Author', description: 'User is a Black author.' }
+  ]
+}
+
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackintech',
+  locales: [
+    { lang: 'en', name: 'Black In Tech', description: 'User works in the tech industry.' }
+  ]
+}
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackentrepreneur',
+  locales: [
+    { lang: 'en', name: 'Black Entrepreneur', description: 'User is a business owner/entrepreneur.' }
+  ]
+}
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackgamer',
+  locales: [
+    { lang: 'en', name: 'Black Gamer', description: 'User is a gamer.' }
+  ]
+}
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackartist',
+  locales: [
+    { lang: 'en', name: 'Black Artist', description: 'User is a person that creates art.' }
+  ]
+}
+{
+  rkey: 'insert-rkey-here',
+  identifier: 'blackmusician',
+  locales: [
+    { lang: 'en', name: 'Black Musician', description: 'User is a musician.' }
+  ]
+}
